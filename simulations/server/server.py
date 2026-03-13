149e0c40b910154937a40541160e2649dff67011
shaurya1795-sys <shaurya1795@gmail.com>
2026-03-13 20:23:33 +0530

    API Created

---
+++ simulations/server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/device-data", methods=["POST"])
+def receive_data():
+    data = request.json
+    print("Received:", data)
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
